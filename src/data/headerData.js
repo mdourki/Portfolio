353ce1ac6b4c9e1a9e5a30099b5ad01c71acea6b
mdourki <dourkimohamed@gmail.com>
2023-01-02 16:51:02 +0100

    update headerData

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Dourki Mohamed',
-    title: "Fullstackk Developer",
+    title: "Fullstack Developer",
     desciption:"Computer Engineering Student enthusiastic and ambitious learner, curious about different fields and always up for new challenges. Seeking an internship opportunity.",
     image: pdp,
     resumePdf: resume
