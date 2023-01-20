e03fb3f5825bfba60d2d8a6843b46edcb6cb2101
mdourki <dourkimohamed@gmail.com>
2023-01-20 18:25:21 +0100

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'dourkimohamed@gmail.com',
     phone: '+212 624863726',
-    address: '33 RUE MESK ELILLE HAY RAHA BEAUSEJOUR, CASABLANCA, MOROCCO, 20200',
+    address: 'Beauséjour, Casablanca, MOROCCO',
 
     sheetAPI: ''
 }
